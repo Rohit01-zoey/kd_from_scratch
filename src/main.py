e39edb904696cfb4cb5b111ff81c8286f56d4214
rohit <rohitkvartak4@gmail.com>
2023-06-28 01:54:35 +0000

    updated

--- src/main.py
+++ src/main.py
@@ -2,15 +2,18 @@ import os
 from torch.utils.data import Dataset
 from torchvision import datasets
 from data import fetch_dataset
+from model import resnet18
 
 
 
+# print(os.getcwd())
+# data_name = 'MNIST'
+# # obj = CIFAR10_data(root =  './src/data/', split = 'train')
+# # obj = eval('{}_data(root = \'./src/data/{}\', split = \'train\')'.format(data_name, data_name))
 
-print(os.getcwd())
-data_name = 'STL10'
-# obj = CIFAR10_data(root =  './src/data/', split = 'train')
-# obj = eval('{}_data(root = \'./src/data/{}\', split = \'train\')'.format(data_name, data_name))
 
+# data = fetch_dataset(data_name)
+# print(data['train'].transform)
 
-data = fetch_dataset(data_name)
-print(data['train'].transform)
+res = resnet18()
+res.summary()
