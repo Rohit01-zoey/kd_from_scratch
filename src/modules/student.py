90069cc511dda2c62c8cb38289c5e49958d552ff
rohit <rohitkvartak4@gmail.com>
2023-06-28 13:52:14 +0000

    modules added

---
+++ src/modules/student.py
@@ -0,0 +1,6 @@
+'''Implementation of the Student model in the KD set up'''
+
+class Student():
+    '''The Student class module for the KD set up'''
+    def __init__(self):
+        pass
---
+++ src/modules/teacher.py
@@ -0,0 +1,6 @@
+'''Implementation of the teacher model in the KD set up'''
+
+class Teacher():
+    '''The Teacher class module for the KD set up'''
+    def __init__(self):
+        pass
