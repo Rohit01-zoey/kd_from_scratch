33de65849c2b69973f9cb032e9a1e06972a58675
rohit <rohitkvartak4@gmail.com>
2023-07-03 01:57:24 +0000

    student added

--- src/modules/student.py
+++ src/modules/student.py
@@ -1,6 +1,21 @@
 '''Implementation of the Student model in the KD set up'''
+import torch.functional as F
+from config import cfg
+from model import *
+
 
 class Student():
     '''The Student class module for the KD set up'''
-    def __init__(self):
-        pass
+    def __init__(self, model, dataset):
+        self.model = model # instantiate the teacher class with the model definition
+        self.dataset = dataset # instantiate the teacher class with the dataset definition
+        
+    def save_model(self, path):
+        '''Save the model'''
+        torch.save(self.model.state_dict(), path)
+        
+    def load_model(self, path):
+        '''Load the model'''
+        self.model.load_state_dict(torch.load(path))
+        self.model.eval()
+        return self.model
